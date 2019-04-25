21087e8679a62b163cede03b1db9fe2514d44a38
Evan Guirino <etg624@gmail.com>
2019-04-25 11:00:08 -0700

    scroll to top on refresh

--- index.js
+++ index.js
@@ -4,40 +4,17 @@ console.log(
 );
 console.log('https://github.com/etg624');
 console.log('https://www.linkedin.com/in/evan-guirino');
-// function anchorLinkHandler(e) {
-//   const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-//   e.preventDefault();
-//   const targetID = this.getAttribute('href');
-//   const targetAnchor = document.querySelector(targetID);
 
-//   if (!targetAnchor) return;
-//   const originalTop = distanceToTop(targetAnchor);
-
-//   window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-
-//   const checkIfDone = setInterval(function() {
-//     const atBottom =
-//       window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-//     if (distanceToTop(targetAnchor) === 0 || atBottom) {
-//       targetAnchor.tabIndex = '-1';
-//       targetAnchor.focus();
-//       window.history.pushState('', '', targetID);
-
-//       clearInterval(checkIfDone);
-//     }
-//   }, 100);
-// }
-
-// const linksToAnchors = document.querySelectorAll('a[href^="#"]');
-
-// linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
+window.onload = () => {
+  window.scrollTo(0, 0);
+};
 
 const typeWriterSections = document.querySelectorAll('.add-typewriter');
 const intersections = new IntersectionObserver(entries => {
   const entry = entries[0];
 
   const interRatio = Math.ceil(entry.intersectionRatio);
-  if (interRatio === 1) {
+  if (interRatio > 0) {
     entry.target.style.opacity = 1;
     entry.target.hidden = false;
     entry.target.classList.add('typewriter');
